7578188386797d0ba2f4e57e7b46918bde4a08cf
yanwenboo <greatgodyan@163.com>
2020-07-02 10:32:09 +0800

    个人中心布局

--- src/components/SelfUserInfo.vue
+++ src/components/SelfUserInfo.vue
@@ -5,12 +5,52 @@
 </template>
 
 <script>
+import { findUserById } from '../api/api'
+
 export default {
   name: 'SelfUserInfo',
-  props: 'home'
+  data () {
+    return {
+      loginInfo: {
+        phoneNumber: '',
+        password: '',
+        id: ''
+      },
+      userModel: {
+        id: '',
+        password: '',
+        checkPass: '',
+        mobile: '',
+        avatar: '',
+        nickName: '',
+        permission: '',
+        isDelete: '',
+        age: '',
+        sex: '',
+        birthday: undefined,
+        email: '',
+        interest: '',
+        idNumber: ''
+      }
+    }
+  },
+  created () {
+    this.getParams()
+  },
+  methods: {
+    getParams () {
+      this.loginInfo.id = localStorage.getItem('userId')
+      findUserById(this.loginInfo).then(data => {
+        if (data.code === 200) {
+          this.userModel = data.data.user
+          console.log(this.userModel)
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
 
 </style>
