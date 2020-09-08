e9f8602cd49a0c25823c1a5deaac20daaedce367
yanwenboo <greatgodyan@163.com>
2020-09-08 11:53:59 +0800

    首页echarts图更换

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-      <div class="echarts-div" id="myChart" :style="{width: this.width, height: '300px'}"></div>
-      <div class="echarts-div" id="myChart5" :style="{width: this.width, height: '300px'}"></div>
-      <div class="echarts-div" id="myChart2" :style="{width: this.width, height: '300px'}"></div>
-      <div class="echarts-div" id="myChart1" :style="{width: this.width, height: '300px'}"></div>
-      <div class="echarts-div" id="myChart8" :style="{width: this.Swidth, height: '300px'}"></div>
+      <div class="echarts-div" id="myChart"  :style="{width: this.width, height: this.height}"></div>
+      <div class="echarts-div" id="myChart5" :style="{width: this.width, height: this.height}"></div>
+      <div class="echarts-div" id="myChart9" :style="{width: this.width, height: this.height}"></div>
+      <div class="echarts-div" id="myChart2" :style="{width: this.width, height: this.height}"></div>
+      <div class="echarts-div" id="myChart8" :style="{width: this.width, height: this.height}"></div>
+      <div class="echarts-div" id="myChart1" :style="{width: this.width, height: this.height}"></div>
   </div>
 </template>
 
@@ -13,8 +14,8 @@ export default {
   name: 'Welcome',
   data () {
     return {
-      width: '425px',
-      Swidth: '660px'
+      width: '420px',
+      height: '300px'
     }
   },
   mounted () {
@@ -23,27 +24,389 @@ export default {
     this.creatC()
     this.creatZ()
     this.creatN()
+    this.creatP()
   },
   methods: {
     creatE () {
       const myChart = this.$echarts.init(document.getElementById('myChart'))
       // 绘制图表
       myChart.setOption({
-        // title: { text: '在Vue中使用echarts' },
-        tooltip: {},
-        xAxis: {
-          data: ['周一', '周二', '周三', '周四', '周五', '周末']
+        title: {
+          // text: '欣酋官网用户访问来源',
+          // subtext: '纯属虚构',
+          left: 'center'
         },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+              { value: 335, name: '直接访问' },
+              { value: 310, name: '邮件营销' },
+              { value: 234, name: '联盟广告' },
+              { value: 135, name: '视频广告' },
+              { value: 1548, name: '搜索引擎' }
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
       })
     },
+
     creatB () {
       const myChart = this.$echarts.init(document.getElementById('myChart1'))
+
+      // 绘制图表
+      myChart.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        legend: {
+          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value'
+        },
+        yAxis: {
+          type: 'category',
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        },
+        series: [
+          {
+            name: '直接访问',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              show: true,
+              position: 'insideRight'
+            },
+            data: [320, 302, 301, 334, 390, 330, 320]
+          },
+          {
+            name: '邮件营销',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              show: true,
+              position: 'insideRight'
+            },
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              show: true,
+              position: 'insideRight'
+            },
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: '视频广告',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              show: true,
+              position: 'insideRight'
+            },
+            data: [150, 212, 201, 154, 190, 330, 410]
+          },
+          {
+            name: '搜索引擎',
+            type: 'bar',
+            stack: '总量',
+            label: {
+              show: true,
+              position: 'insideRight'
+            },
+            data: [820, 832, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      })
+    },
+    creatC () {
+      const myChart = this.$echarts.init(document.getElementById('myChart2'))
+      // 绘制图表
+      myChart.setOption({
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        legend: {
+          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎', '百度', '谷歌', '必应', '其他']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '直接访问',
+            type: 'bar',
+            data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name: '邮件营销',
+            type: 'bar',
+            stack: '广告',
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'bar',
+            stack: '广告',
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: '视频广告',
+            type: 'bar',
+            stack: '广告',
+            data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name: '搜索引擎',
+            type: 'bar',
+            data: [862, 1018, 964, 1026, 1679, 1600, 1570],
+            markLine: {
+              lineStyle: {
+                type: 'dashed'
+              },
+              data: [
+                [{ type: 'min' }, { type: 'max' }]
+              ]
+            }
+          },
+          {
+            name: '百度',
+            type: 'bar',
+            barWidth: 5,
+            stack: '搜索引擎',
+            data: [620, 732, 701, 734, 1090, 1130, 1120]
+          },
+          {
+            name: '谷歌',
+            type: 'bar',
+            stack: '搜索引擎',
+            data: [120, 132, 101, 134, 290, 230, 220]
+          },
+          {
+            name: '必应',
+            type: 'bar',
+            stack: '搜索引擎',
+            data: [60, 72, 71, 74, 190, 130, 110]
+          },
+          {
+            name: '其他',
+            type: 'bar',
+            stack: '搜索引擎',
+            data: [62, 82, 91, 84, 109, 110, 120]
+          }
+        ]
+      })
+    },
+    creatZ () {
+      const myChart = this.$echarts.init(document.getElementById('myChart5'))
+      // 绘制图表
+      myChart.setOption({
+        title: {
+        // text: '浏览器占比变化',
+        // subtext: '纯属虚构',
+          top: 10,
+          left: 10
+        },
+        tooltip: {
+          trigger: 'item',
+          backgroundColor: 'rgba(0,0,250,0.2)'
+        },
+        legend: {
+          type: 'scroll',
+          bottom: 10,
+          data: (function () {
+            var list = []
+            for (var i = 1; i <= 28; i++) {
+              list.push(i + 2000 + '')
+            }
+            return list
+          })()
+        },
+        visualMap: {
+          top: 'middle',
+          right: 10,
+          color: ['red', 'yellow'],
+          calculable: true
+        },
+        radar: {
+          indicator: [
+            { text: 'IE8-', max: 400 },
+            { text: 'IE9+', max: 400 },
+            { text: 'Safari', max: 400 },
+            { text: 'Firefox', max: 400 },
+            { text: 'Chrome', max: 400 }
+          ]
+        },
+        series: (function () {
+          var series = []
+          for (var i = 1; i <= 28; i++) {
+            series.push({
+              name: '浏览器（数据纯属虚构）',
+              type: 'radar',
+              symbol: 'none',
+              lineStyle: {
+                width: 1
+              },
+              emphasis: {
+                areaStyle: {
+                  color: 'rgba(0,250,0,0.3)'
+                }
+              },
+              data: [{
+                value: [
+                  (40 - i) * 10,
+                  (38 - i) * 4 + 60,
+                  i * 5 + 10,
+                  i * 9,
+                  i * i / 2
+                ],
+                name: i + 2000 + ''
+              }]
+            })
+          }
+          return series
+        })()
+      })
+    },
+    creatN () {
+      const myChart = this.$echarts.init(document.getElementById('myChart8'))
+
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          // text: '堆叠区域图'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#6a7985'
+            }
+          }
+        },
+        legend: {
+          data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: '视频广告',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name: '直接访问',
+            type: 'line',
+            stack: '总量',
+            areaStyle: {},
+            data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name: '搜索引擎',
+            type: 'line',
+            stack: '总量',
+            label: {
+              normal: {
+                show: true,
+                position: 'top'
+              }
+            },
+            areaStyle: {},
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      })
+    },
+    creatP () {
+      const myChart = this.$echarts.init(document.getElementById('myChart9'))
       // 绘制图表
       myChart.setOption({
         angleAxis: {
@@ -79,206 +442,12 @@ export default {
           data: ['A', 'B', 'C']
         }
       })
-    },
-    creatC () {
-      const myChart = this.$echarts.init(document.getElementById('myChart2'))
-      // 绘制图表
-      myChart.setOption({
-        xAxis: {},
-        yAxis: {},
-        series: [{
-          symbolSize: 20,
-          data: [
-            [10.0, 8.04],
-            [8.0, 6.95],
-            [13.0, 7.58],
-            [9.0, 8.81],
-            [11.0, 8.33],
-            [14.0, 9.96],
-            [6.0, 7.24],
-            [4.0, 4.26],
-            [12.0, 10.84],
-            [7.0, 4.82],
-            [5.0, 5.68],
-            [1.0, 1.0],
-            [3.0, 8.0],
-            [4.5, 10.0],
-            [7.5, 10.0],
-            [9.0, 8.0],
-            [6.0, 8.0],
-            [3.5, 6.0],
-            [4.0, 4.0],
-            [6.0, 2.5]
-          ],
-          type: 'scatter'
-        }]
-      })
-    },
-    creatZ () {
-      const myChart = this.$echarts.init(document.getElementById('myChart5'))
-      // 绘制图表
-      myChart.setOption({
-        // 当前插件的背景图
-        backgroundColor: '#dfe9fb',
-        title: {
-          text: '登录方式',
-          left: 'center',
-          top: 20,
-          textStyle: {
-            color: '#874429'
-          }
-        },
-
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-
-        visualMap: {
-          show: false,
-          min: 80,
-          max: 600,
-          inRange: {
-            colorLightness: [0, 1]
-          }
-        },
-        series: [
-          {
-            name: '访问来源',
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '50%'],
-            data: [
-              { value: 335, name: '直接访问' },
-              { value: 310, name: '邮件营销' },
-              { value: 274, name: '联盟广告' },
-              { value: 235, name: '视频广告' },
-              { value: 400, name: '搜索引擎' }
-            ].sort(function (a, b) { return a.value - b.value }),
-            roseType: 'radius',
-            label: {
-              color: '#874429'
-            },
-            labelLine: {
-              lineStyle: {
-                color: '#874429'
-              },
-              smooth: 0.2,
-              length: 10,
-              length2: 20
-            },
-            itemStyle: {
-              color: '#c23531',
-              shadowBlur: 200,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            },
-
-            animationType: 'scale',
-            animationEasing: 'elasticOut',
-            animationDelay: function (idx) {
-              return Math.random() * 200
-            }
-          }
-        ]
-      })
-    },
-    creatN () {
-      const myChart = this.$echarts.init(document.getElementById('myChart8'))
-      // 绘制图表
-      myChart.setOption({
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b}: {c} ({d}%)'
-        },
-        legend: {
-          orient: 'vertical',
-          left: 10,
-          data: ['直达', '营销广告', '搜索引擎', '邮件营销', '联盟广告', '视频广告', '百度', '谷歌', '必应', '其他']
-        },
-        series: [
-          {
-            name: '访问来源',
-            type: 'pie',
-            selectedMode: 'single',
-            radius: [0, '30%'],
-
-            label: {
-              position: 'inner'
-            },
-            labelLine: {
-              show: false
-            },
-            data: [
-              { value: 335, name: '直达', selected: true },
-              { value: 679, name: '营销广告' },
-              { value: 1548, name: '搜索引擎' }
-            ]
-          },
-          {
-            name: '访问来源',
-            type: 'pie',
-            radius: ['40%', '55%'],
-            label: {
-              formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
-              backgroundColor: '#eee',
-              borderColor: '#aaa',
-              borderWidth: 1,
-              borderRadius: 4,
-              // shadowBlur:3,
-              // shadowOffsetX: 2,
-              // shadowOffsetY: 2,
-              // shadowColor: '#999',
-              // padding: [0, 7],
-              rich: {
-                a: {
-                  color: '#999',
-                  lineHeight: 22,
-                  align: 'center'
-                },
-                // abg: {
-                //     backgroundColor: '#333',
-                //     width: '100%',
-                //     align: 'right',
-                //     height: 22,
-                //     borderRadius: [4, 4, 0, 0]
-                // },
-                hr: {
-                  borderColor: '#aaa',
-                  width: '100%',
-                  borderWidth: 0.5,
-                  height: 0
-                },
-                b: {
-                  fontSize: 16,
-                  lineHeight: 33
-                },
-                per: {
-                  color: '#eee',
-                  backgroundColor: '#334455',
-                  padding: [2, 4],
-                  borderRadius: 2
-                }
-              }
-            },
-            data: [
-              { value: 335, name: '直达' },
-              { value: 310, name: '邮件营销' },
-              { value: 234, name: '联盟广告' },
-              { value: 135, name: '视频广告' },
-              { value: 1048, name: '百度' },
-              { value: 251, name: '谷歌' },
-              { value: 147, name: '必应' },
-              { value: 102, name: '其他' }
-            ]
-          }
-        ]
-      })
     }
   }
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 .echarts-div {
   float: left;
   display: inline;
